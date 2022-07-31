c3d5922636321238dd237ee0c4e3fbea79cb74ac
Nceba <nceba.dumasi@gmail.com>
2022-07-31 17:53:47 +0200

    launch

---
+++ backend/src/main/java/com/waxes27/sHome/SHomeApplication.java
@@ -0,0 +1,13 @@
+package com.waxes27.sHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SHomeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/waxes27/sHome/SHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waxes27.sHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
